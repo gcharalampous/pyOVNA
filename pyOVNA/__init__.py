e4f82bfd7b72ccc5a40cee271dedcdb80c21ee32
Georgios Charalampous <gcharalampous@ucdavis.edu>
2025-04-26 15:57:20 -0700

    Added plot_ovna_data into __init__

--- pyOVNA/__init__.py
+++ pyOVNA/__init__.py
@@ -1,4 +1,4 @@
 from .io import OVNAREADER
 from .filters import supergf, rectf, filter_data
-from .processing import read_ovna_file
+from .processing import read_ovna_file, plot_ovna_data
 from .fitting import fit_spectrum_peaks, fit_propagation_length, fano_resonance_equation, fit_fano_resonance, calculate_q_numerically
